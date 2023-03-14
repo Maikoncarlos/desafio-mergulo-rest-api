66a2e1c1aba32d402cadfc77df4921cd1a37228d
Maikoncarlos <maikonsaturno@gmail.com>
2023-03-14 08:42:47 -0300

    criação projeto

---
+++ src/main/java/com/github/maikoncarlos/desafio_mergulho_rest_api/DesafioMergulhoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.desafio_mergulho_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMergulhoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMergulhoRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maikoncarlos/desafio_mergulho_rest_api/DesafioMergulhoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.desafio_mergulho_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMergulhoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
